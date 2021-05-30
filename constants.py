209ce4b46a819340ca5f2b0135555716d97442b4
Sanchit Balchandani <balchandani.sanchit@gmail.com>
2021-05-30 21:11:07 +0530

    Delete constants.py

--- constants.py
+++
@@ -1,2 +0,0 @@
-URL_pc = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode={}&date={}"
-URL_city = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/calendarByDistrict?district_id={}&date={}"
